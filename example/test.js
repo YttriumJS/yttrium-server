447f97aa57309fe55e71afd81d71ac495bf16a40
Peter <psquicciarini@redventures.com>
2017-08-14 10:05:02 -0400

    Updated example

--- example/test.js
+++ example/test.js
@@ -4,50 +4,65 @@ const { $, server, router } = yt;
 
 // add a test route on the root (e.g. localhost/test)
 $.route('index')
-  .append('<get>')
-  .append('<post>')
-  .append('<test>')
+  .append('<get-example>')
+  .append('<post-example>')
+  .append('<html-example>')
   .append('<nested data-dynamic="name">');
 
 // add handler to index route
 $.route('index')
   .on('route', (e, req, res) => {
+    // it's important to stop propagation of the route event
+    // unless you want to trigger the parental route functions
+    // which can be useful in middleware situations
+    e.stopPropagation();
     return res.end('Index route!');
   });
 
 // http://localhost:8000/get?poop=shoot
-$.route('get')
+$.route('get-example')
   .on('route', (e, req, res) => {
-    const poopParam = $.route('querystring').find('poop').text();
-    return res.end(`Get params: poop is ${poopParam}`);
+    e.stopPropagation();
+    const testParam = $.route('querystring').find('test').text();
+    if (!testParam) {
+      return res.end('Add a query string parameter of test to this example like /get-example?test=happiness');
+    }
+    return res.end(`Get params: test is ${testParam}`);
   });
 
 // http://localhost:8000/post
 // Body: 'cabbage'
-$.route('post')
+$.route('post-example')
   .on('route', (e, req, res) => {
-    // What is life?
     e.stopPropagation();
 
     // Parse the body
     $.body(req)
     .then((body) => {
+      if (!body) {
+        return res.end('Send a post body to this example')
+      }
       return res.end(`Post body: ${body}`);
     });
   });
 
 // add handler to test route
-$.route('test')
+$.route('html-example')
   .on('route', (e, req, res) => {
+    e.stopPropagation();
     // example of sending HTML template
-    $('body').html('<div>Hello world!</div>');
+    $('body').html('<h1>This example uses HTML</h1>');
     return res.end($('html').html());
   });
 
 // this route responds to /nested/:name
 $.route('nested')
   .on('route', (e, req, res) => {
+    e.stopPropagation();
     const param = $.route('nested').data('name');
+    if (!param) {
+      return res.end('Navigate to /nested/anything to see dynamic routes in action')
+    }
     return res.end(`I found a name! ${param}`);
   });
 
